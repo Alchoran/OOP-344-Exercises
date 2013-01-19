b6a0fa20dd1366abf17949fa2c4ffe7f96d888d9
Lucas McIntosh <lhmcintosh@myseneca.ca>
2013-01-19 11:12:22 -0500

    Fixed prnenv returning partial matches

--- prnenv.cpp
+++ prnenv.cpp
@@ -1,7 +1,10 @@
 ///PRNENV.CPP///
 // Lucas McIntosh //
 // 19/01/2013 //
-// Searches the system environment variables for the specified argument passed from the cl
+/*
+ Searches the system environment variables for the specified argument
+ passed from the cl and prints out it's value.
+*/
 
 #include <iostream>
 #include <cstring>
@@ -9,18 +12,22 @@
 using namespace std;
 
 int main(int argc, char* argv[], char* env[]){
+	
 	char* pch = NULL;
+	char* pch2 = NULL;
 	int x=0;
-	char str[strlen(argv[1]+1]);
+	char str[strlen(argv[1]+1)];
 	
 	for(x=0;argv[1][x]!=0;str[x]=toupper(argv[1][x]),x++);
 	str[x]=0;
 	
-	for(x=0;env[x]!=0 && pch==NULL ; x++){
-		pch=strstr(env[x], str);
+	for(x=0;env[x]!=0 && pch==NULL;pch=strstr(env[x], str), x++);
+		
+	if(pch){
+		pch2 = strtok (env[--x], "=");
+		if(strlen(pch2) == strlen(str))
+			cout <<  env[x] << endl;
+		else
+			cout << "not found" << endl;
 	}
-	if(pch)
-		cout <<  env[x-1] << endl;
-	else
-		cout << "not found" << endl;
 }
